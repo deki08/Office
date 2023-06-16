7dba5c22c09d5930bc6e7a0774f11902350bdb42
deki08 <teamsoul234@gmail.com>
2023-06-16 17:55:25 +0530

    Shecduleer

--- Moov/src/main/java/com/moov/Moov/Scheduler/ThreadScheduler.java
+++ Moov/src/main/java/com/moov/Moov/Scheduler/ThreadScheduler.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.moov.Moov.model.TblBilling;
 import com.moov.Moov.service.SubService;
 
 @Component
+@Service
 public class ThreadScheduler extends Thread {
 
 	@Autowired
